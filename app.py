f79331fbf171528c13d26a82eba94768a846978c
admin <admin@my.io>
2020-06-11 13:32:27 +0800

    增加用户管理相关路由

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from wsgiref.simple_server import make_server
 from blog import config
 from magweb import MagWeb
+from blog.handler.user import user_router
 
 
 if __name__ == '__main__':
     applications = MagWeb()
+
+    # 用户注册，登陆相关的路由注册
+    applications.register(user_router)
+
     server = make_server(config.WSIP, config.WSPORT, applications)
     try:
         server.serve_forever()
