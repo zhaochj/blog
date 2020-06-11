80edceb471aed15e833798fe00b7a5cb2c1a9d0d
admin <admin@my.io>
2020-06-11 22:05:18 +0800

    增加博文发布及查询接口

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from wsgiref.simple_server import make_server
 from blog import config
 from magweb import MagWeb
 from blog.handler.user import user_router
+from blog.handler.post import post_router
 
 
 if __name__ == '__main__':
-    applications = MagWeb()
+    application = MagWeb()
 
     # 用户注册，登陆相关的路由注册
-    applications.register(user_router)
+    application.register(user_router)
+    application.register(post_router)
 
-    server = make_server(config.WSIP, config.WSPORT, applications)
+    server = make_server(config.WSIP, config.WSPORT, application)
     try:
         server.serve_forever()
     except KeyboardInterrupt:
---
+++ blog/handler/post.py
@@ -0,0 +1,68 @@
+from magweb import MagWeb
+from .user import authenticate
+from ..model import session, Post, Content
+from webob import exc
+from ..util import jsonify
+import datetime
+
+
+# 与文章的路由
+post_router = MagWeb.Router(prefix='/post')
+
+
+# 文章发布接口 /post/
+
+@post_router.post('/')   # pub = post_router.post('/')(pub)
+@authenticate  # 验证
+def pub(ctx, request: MagWeb.Request):
+    # 用户要发布文章： 1. 需要登陆，需要验证用户， 2. 需要title, content
+    payload = request.json
+
+    post = Post()
+    try:
+        post.title = payload.get('title')
+        post.author_id = request.user.id  # 通过@authenticate验证后会在request上绑定user实例
+        post.postdate = datetime.datetime.now()
+
+        # content实际存放在Content实体对象中，需要使用以下方式绑定数据
+        _content = Content()
+        _content.content = payload['content']
+        post.content = _content
+    except Exception as e:
+        print(2, e)
+        raise exc.HTTPBadRequest()
+
+    session.add(post)
+    try:
+        session.commit()
+        return jsonify(post_id=post.id)
+    except:
+        session.rollback()
+        raise exc.HTTPInternalServerError()
+
+
+# 查看单个博文
+@post_router.get('/{id:int}')
+def get(ctx, request: MagWeb.Request) -> MagWeb.Response:
+    p_id = request.vars.id
+    try:
+        post = session.query(Post).get(p_id)  # 可以使用下边语句
+        # post = session.query(Post).filter(Post.id == p_id).first()
+        return jsonify(post={
+            'post_id': post.id,
+            'title': post.title,
+            'author': post.author.name,
+            'author_id': post.author_id,
+            'postdate': post.postdate.timestamp(),  # 需要转换为timestamp，否则报Object of type 'datetime' is not JSON serializable
+            'content': post.content.content
+        })
+    except Exception as e:
+        print(e)
+        raise exc.HTTPNotFound()
+
+
+
+
+
+
+
