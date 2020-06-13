415731b9d3ce700a8784d86ae0411e4832c46267
admin <admin@my.io>
2020-06-14 00:56:08 +0800

    前端实现登陆功能，数据异常发送到后端。

--- fe/src/component/Login.js
+++ fe/src/component/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/login.css'
+import UserService from "../service/user";
 
+const service = new UserService();
 
 export default class Login extends React.Component {
+    render() {
+        return <_Login service={service} />;  {/*采用这种方式注入props*/}
+    }
+}
+
+
+class _Login extends React.Component {
+    handleClick(event) {
+        event.preventDefault();  // form方式提交数据时，需要阻止默认的刷新操作
+        const fm = event.target.form;
+        let email = fm[0].value;
+        let pwd = fm[1].value;
+        // 获取到用户名及密码后，需要异步调用后端的登陆接口。这里还是要想办法使用props的方式调用
+        this.props.service.login(email, pwd)
+
+    }
+
     render() {
         return (
             <div className="login-page">
@@ -11,7 +30,7 @@ export default class Login extends React.Component {
                     <form className="login-form">
                         <input type="text" placeholder="邮箱"/>
                         <input type="password" placeholder="密码"/>
-                        <button>登陆</button>
+                        <button onClick={this.handleClick.bind(this)}>登陆</button>
                         <p className="message">还未注册? <Link to="/reg">请注册</Link></p>
                     </form>
                 </div>
